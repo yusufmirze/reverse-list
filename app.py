cd10bdfcdacf949ffc8f17887213126ea46c7deb
Yusuf Mirze <66483412+yusufmirze@users.noreply.github.com>
2023-08-25 00:19:27 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+arr = [8,11,5,7,9,4,12,13]
+def reverse(): 
+    k = 0 #starting int
+    #printing length
+    index = 0
+    for i in arr:
+        index +=1
+    print(index) 
+    for i in arr:
+        k -= 1
+        print(arr[k], end=' ')
+    x = arr[-1]
+        #arr[-]
+reverse()
